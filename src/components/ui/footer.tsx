c8c919ad9b3a164ce303d30be3ae2c3163e5a495
Emmanuel Agüero <54730752+EmmanuelAR@users.noreply.github.com>
2025-03-21 13:02:05 -0600

    #118 Refesh titles size and media logos

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -4,19 +4,15 @@ const Footer = (): React.JSX.Element => {
 	return (
 		<footer className="text-white mt-auto">
 			<div className="w-full border-t border-gray-800"></div>
-			{/* Newsletter Section */}
 			<div className="container mx-auto p-7">
 				<div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-					{/* Newsletter Signup */}
 					<div className="md:col-span-1 flex flex-col items-center text-center">
 						<h2 className="text-2xl font-bold mb-4">
 							Don't Miss Out
 						</h2>
-
-						{/* Social Media Icons - Using original icons */}
 						<div className="flex space-x-2 mt-6">
 							<a
-								className="p-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors"
+								className="p-2 hover:bg-white hover:text-black transition-colors"
 								href="https://github.com/undefinedorgcr"
 								target="_blank"
 							>
@@ -40,7 +36,7 @@ const Footer = (): React.JSX.Element => {
 								</svg>
 							</a>
 							<a
-								className="p-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors"
+								className="p-2 hover:bg-white hover:text-black transition-colors"
 								href="https://x.com/lumosapp_"
 								target="_blank"
 							>
@@ -56,7 +52,7 @@ const Footer = (): React.JSX.Element => {
 								</svg>
 							</a>
 							<a
-								className="p-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors"
+								className="p-2 hover:bg-white hover:text-black transition-colors"
 								href="https://discord.gg/sEpnC6JB2U"
 								target="_blank"
 							>
@@ -70,9 +66,8 @@ const Footer = (): React.JSX.Element => {
 									<path d="M20.317,4.37c-1.53-0.702-3.17-1.219-4.885-1.515c-0.031-0.006-0.062,0.009-0.079,0.037   c-0.211,0.375-0.445,0.865-0.608,1.249c-1.845-0.276-3.68-0.276-5.487,0C9.095,3.748,8.852,3.267,8.641,2.892   C8.624,2.864,8.593,2.85,8.562,2.855C6.848,3.15,5.208,3.667,3.677,4.37C3.664,4.375,3.652,4.385,3.645,4.397   c-3.111,4.648-3.964,9.182-3.546,13.66c0.002,0.022,0.014,0.043,0.031,0.056c2.053,1.508,4.041,2.423,5.993,3.029   c0.031,0.01,0.064-0.002,0.084-0.028c0.462-0.63,0.873-1.295,1.226-1.994c0.021-0.041,0.001-0.09-0.042-0.106   c-0.653-0.248-1.274-0.55-1.872-0.892c-0.047-0.028-0.051-0.095-0.008-0.128c0.126-0.094,0.252-0.192,0.372-0.291   c0.022-0.018,0.052-0.022,0.078-0.01c3.928,1.793,8.18,1.793,12.061,0c0.026-0.012,0.056-0.009,0.079,0.01   c0.12,0.099,0.246,0.198,0.373,0.292c0.044,0.032,0.041,0.1-0.007,0.128c-0.598,0.349-1.219,0.645-1.873,0.891   c-0.043,0.016-0.061,0.066-0.041,0.107c0.36,0.698,0.772,1.363,1.225,1.993c0.019,0.027,0.053,0.038,0.084,0.029   c1.961-0.607,3.95-1.522,6.002-3.029c0.018-0.013,0.029-0.033,0.031-0.055c0.5-5.177-0.838-9.674-3.548-13.66   C20.342,4.385,20.33,4.375,20.317,4.37z M8.02,15.331c-1.183,0-2.157-1.086-2.157-2.419s0.955-2.419,2.157-2.419   c1.211,0,2.176,1.095,2.157,2.419C10.177,14.246,9.221,15.331,8.02,15.331z M15.995,15.331c-1.182,0-2.157-1.086-2.157-2.419   s0.955-2.419,2.157-2.419c1.211,0,2.176,1.095,2.157,2.419C18.152,14.246,17.206,15.331,15.995,15.331z" />
 								</svg>
 							</a>
-							{/* Telegram Icon */}
 							<a
-								className="p-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors"
+								className="p-2 hover:bg-white hover:text-black transition-colors"
 								href="https://t.me/+zB6wXUOsWptjNzQx"
 								target="_blank"
 							>
@@ -88,24 +83,18 @@ const Footer = (): React.JSX.Element => {
 							</a>
 						</div>
 					</div>
-
-					{/* Company Info */}
 					<div className="md:col-span-1 flex flex-col items-center text-center ">
-						<h3 className="text-lg font-medium mb-4 uppercase">
-							Company
-						</h3>
+						<h3 className="text-lg font-bold mb-4 ">Company</h3>
 						<ul className="space-y-2">
 							<li>
-								<a href="#" className="hover:underline">
+								<a href="/knowmore" className="hover:underline">
 									About
 								</a>
 							</li>
 						</ul>
 					</div>
-
-					{/* Customer Service */}
 					<div className="md:col-span-1 flex flex-col items-center text-center">
-						<h3 className="text-lg font-medium mb-4 uppercase ">
+						<h3 className="text-lg font-bold mb-4  ">
 							Customer Service
 						</h3>
 						<ul className="space-y-2">
@@ -124,10 +113,8 @@ const Footer = (): React.JSX.Element => {
 							</li>
 						</ul>
 					</div>
-
-					{/* More to Explore */}
 					<div className="md:col-span-1 flex flex-col items-center text-center">
-						<h3 className="text-lg font-medium mb-4 uppercase">
+						<h3 className="text-lg font-bold mb-4 ">
 							More to Explore
 						</h3>
 						<ul className="space-y-2">
@@ -156,8 +143,6 @@ const Footer = (): React.JSX.Element => {
 					</div>
 				</div>
 			</div>
-
-			{/* Bottom Links - Modificado para tener línea en todo el ancho y texto centrado */}
 			<div>
 				<div className="container mx-auto px-8 py-4">
 					<div className="flex justify-center items-center">
